54f12beeb0d270f169ffae7585274870b3d0255d
wavyStructures <anja_schwab@gmx.de>
2025-06-29 21:26:19 +0200

    update

--- offers_app/serializers.py
+++ offers_app/serializers.py
@@ -216,27 +216,35 @@ class OfferSerializer(serializers.ModelSerializer):
         except Exception as e:
             raise serializers.ValidationError({"internal_error": str(e)})
 
-    def update(self, instance, validated_data):
-        details_data = validated_data.pop("details", None)
-        
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
+def update(self, instance, validated_data):
+    details_data = validated_data.pop("details", None)
+
+    for attr, value in validated_data.items():
+        setattr(instance, attr, value)
+    instance.save()
+
+    if details_data is not None:
+        existing_details = {detail.id: detail for detail in instance.details.all()}
+
+        for raw_detail_data in details_data:
+            detail_data = dict(raw_detail_data)
+            detail_id = detail_data.get("id")
+
+            if detail_id and detail_id in existing_details:
+                detail_instance = existing_details[detail_id]
+                for attr, value in detail_data.items():
+                    if attr != "id":
+                        setattr(detail_instance, attr, value)
+                detail_instance.save()
+            else:
+                OfferDetail.objects.create(offer=instance, **detail_data)
+
+    all_details = instance.details.all()
+    if all_details.exists():
+        instance.min_price = min(d.price for d in all_details)
+        instance.min_delivery_time = min(d.delivery_time_in_days for d in all_details)
         instance.save()
-        
-        if details_data is not None:
-            instance.details.all().delete()
 
-            detail_objs = []
-            for detail_data in details_data:
-                detail = OfferDetail.objects.create(offer=instance, **detail_data)
-                detail_objs.append(detail)
-
-            if detail_objs:
-                instance.min_price = min(d.price for d in detail_objs)
-                instance.min_delivery_time = min(d.delivery_time_in_days for d in detail_objs)
-                instance.save()        
-
-        return instance
-
+    return instance
 
 
